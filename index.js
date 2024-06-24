7aadd7009287ab898dde7433acb4afe51adc9201
MateoBarcelo <mateobarcelo88@gmail.com>
2024-06-24 08:34:09 -0300

    feat: Add import for initial sounds

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const sounds = [
-  {
-    name: "Ring",
-    url: "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a",
-    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95b91a82-dea0-45f0-a1f7-e69e683ce44c/df33wc2-83a686a8-e9f8-4843-ab9c-5de157b51154.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk1YjkxYTgyLWRlYTAtNDVmMC1hMWY3LWU2OWU2ODNjZTQ0Y1wvZGYzM3djMi04M2E2ODZhOC1lOWY4LTQ4NDMtYWI5Yy01ZGUxNTdiNTExNTQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R1Bun3TRHhRbkJBb1egdgZkqBnxp5Yhx9qfBavzzu3c",
-  },
-  {
-    name: "Alien shoot",
-    url: "http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_shoot.wav",
-    img: "https://as2.ftcdn.net/v2/jpg/05/14/21/65/1000_F_514216598_gdfGlO1JXlDZea7k0cBYL15LJFKuH4oj.jpg",
-  },
-];
+import { sounds } from "./dummy/sounds.js";
 
 class Soundboard {
   constructor() {
