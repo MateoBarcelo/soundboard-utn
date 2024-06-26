3b9e43c9a3566c1baa96153d0dc47a7dbc541b29
MateoBarcelo <mateobarcelo88@gmail.com>
2024-06-26 15:55:24 -0300

    chore: Update .gitignore and remove tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#E8D849",
+        "secondary": "#212121",
+        "secondary-dark": "#161616",
+      },
+    },
+  },
+  plugins: [],
+}
+
