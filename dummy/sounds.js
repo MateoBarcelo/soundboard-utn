22c31991561e34f7fe3562418ecfe2b1aaf80cc7
MateoBarcelo <mateobarcelo88@gmail.com>
2024-06-26 17:21:45 -0300

    feat: add more sounds

--- dummy/sounds.js
+++ dummy/sounds.js
@@ -1,13 +1,42 @@
 export const sounds = [
-    {
-        name: 'Ring',
-        url: 'http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a',
-        img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95b91a82-dea0-45f0-a1f7-e69e683ce44c/df33wc2-83a686a8-e9f8-4843-ab9c-5de157b51154.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk1YjkxYTgyLWRlYTAtNDVmMC1hMWY3LWU2OWU2ODNjZTQ0Y1wvZGYzM3djMi04M2E2ODZhOC1lOWY4LTQ4NDMtYWI5Yy01ZGUxNTdiNTExNTQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R1Bun3TRHhRbkJBb1egdgZkqBnxp5Yhx9qfBavzzu3c"
-    },
-    {
-        name: 'Alien shoot',
-        url: 'http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_shoot.wav',
-        img: "https://as2.ftcdn.net/v2/jpg/05/14/21/65/1000_F_514216598_gdfGlO1JXlDZea7k0cBYL15LJFKuH4oj.jpg"
-    
-    }
-]
+  {
+    name: "Ring",
+    url: "http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a",
+    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/95b91a82-dea0-45f0-a1f7-e69e683ce44c/df33wc2-83a686a8-e9f8-4843-ab9c-5de157b51154.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzk1YjkxYTgyLWRlYTAtNDVmMC1hMWY3LWU2OWU2ODNjZTQ0Y1wvZGYzM3djMi04M2E2ODZhOC1lOWY4LTQ4NDMtYWI5Yy01ZGUxNTdiNTExNTQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.R1Bun3TRHhRbkJBb1egdgZkqBnxp5Yhx9qfBavzzu3c",
+  },
+  {
+    name: "Alien shoot",
+    url: "http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_shoot.wav",
+    img: "https://as2.ftcdn.net/v2/jpg/05/14/21/65/1000_F_514216598_gdfGlO1JXlDZea7k0cBYL15LJFKuH4oj.jpg",
+  },
+  {
+    name: "Salto",
+    url: "https://commondatastorage.googleapis.com/codeskulptor-assets/jump.ogg",
+    img: "https://i.pinimg.com/originals/4f/ee/e7/4feee7d38d062093cdd532c01139f5a2.gif",
+  },
+  {
+    name: "Bonus",
+    url: "http://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/bonus.wav",
+    img: "https://media.istockphoto.com/id/1289940495/vector/pixel-art-gui-sign-with-red-ribbon-and-treasure-chest.jpg?s=612x612&w=0&k=20&c=VASMn1mZ1cetAdlZelBLZNwWb9yQHvJmKyaFXXkCgKU="
+  },
+  {
+    name: "Disparo de flecha",
+    url: "http://codeskulptor-demos.commondatastorage.googleapis.com/pang/arrow.mp3",
+    img: "https://i.redd.it/7ka6sxjj0e961.gif"
+  },
+  {
+    name: "Caida",
+    url: "http://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/theygotcha.ogg",
+    img: "https://art.pixilart.com/sr28bd3cce8a650.gif"
+  },
+  {
+    name: "Guitarra",
+    url: "http://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/music/start.ogg",
+    img: "https://img.freepik.com/vector-premium/guitarra-acustica-estilo-pixel-art_475147-346.jpg"
+  },
+  {
+    name: "Explosion",
+    url: "http://commondatastorage.googleapis.com/codeskulptor-assets/sounddogs/explosion.ogg",
+    img: "https://as1.ftcdn.net/v2/jpg/00/84/60/06/1000_F_84600669_fJE6VL1JqbmvzQ7L3wCBzMJC2MlsFgCk.jpg"
+  }
+];
